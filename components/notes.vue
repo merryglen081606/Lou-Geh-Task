5fc2463e3b2c34afb831c06e49bb3b62d57214e0
Kenn Brian Nillama <kenn.brian.nillama@biotechfarms.net>
2022-10-20 16:51:59 +0800

    Second commit

--- components/notes.vue
+++ components/notes.vue
@@ -29,7 +29,7 @@ created() {
       {
         id: "n"+x,
         title: "Note no. "+x,
-        desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        desc: "Lorem Ipsum is simply dummy text of the xprinting and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
         status: "To Do"
       }
     );
